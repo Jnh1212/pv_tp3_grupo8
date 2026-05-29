7f7d94202621b5c314dc2d4de7e5abc81d378f27
maxisarapura <maxisarapura@gmail.com>
2026-05-29 16:40:44 -0300

    Correcion de algunos detalles y agregado de css para la pestaña de detalles

--- projectTeam8/src/components/ProyectoCard.jsx
+++ projectTeam8/src/components/ProyectoCard.jsx
@@ -1,15 +1,23 @@
-const ProyectoCard = ({ proyecto, onEliminar }) => {
+import "./../css/estilos.css";
+
+const ProyectoCard = ({ proyecto, onEliminar, onVerDetalle }) => {
   const { titulo, categoria, estado, id } = proyecto;
 
   return (
     <div className="proyecto">
       <h3>{titulo}</h3>
-      <p>Categoría: {categoria}</p>
-      <p>Estado: {estado}</p>
-      <button className="boton" onClick={() => onEliminar(id)}>
+      <p>
+        <strong>Categoría:</strong> {categoria}
+      </p>
+      <p>
+        <strong>Estado:</strong> {estado}
+      </p>
+      <button className="boton_eliminar" onClick={() => onEliminar(id)}>
         Eliminar
       </button>
-      <button>Ver detalle</button>
+      <button className="boton_detalle" onClick={onVerDetalle}>
+        Ver detalle
+      </button>
     </div>
   );
 };
