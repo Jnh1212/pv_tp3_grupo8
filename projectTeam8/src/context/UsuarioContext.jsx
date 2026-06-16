7f7768ea4d9b8d1f9f63f192766a007031ef73e1
maxisarapura <maxisarapura@gmail.com>
2026-06-16 15:24:09 -0300

    Persistencia del estado global act 3 agregado

--- projectTeam8/src/context/UsuarioContext.jsx
+++ projectTeam8/src/context/UsuarioContext.jsx
@@ -1,17 +1,29 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 // 1. Crear el contexto
 export const UsuarioContext = createContext();
 
+const usuarioPorDefecto = {
+  nombre: "Juan Pérez",
+  dni: "12345678",
+  rol: "Alumno",
+  institucion: "Escuela N°168",
+};
 // 2. Crear el proveedor
 export const UsuarioProvider = ({ children }) => {
   // Estado inicial simulado (usuario ya logueado)
-  const [usuario, setUsuario] = useState({
-    nombre: "Juan Pérez",
-    dni: "12345678",
-    rol: "Alumno",
-    institucion: "Escuela N°168"
+  const [usuario, setUsuario] = useState(() => {
+    const usuarioGuardado = localStorage.getItem("usuario");
+
+    if (usuarioGuardado) {
+      return JSON.parse(usuarioGuardado);
+    }
+    return usuarioPorDefecto;
   });
+
+  useEffect(() => {
+    localStorage.setItem("usuario", JSON.stringify(usuario));
+  }, [usuario]);
 
   // Función para actualizar perfil
   const actualizarPerfil = (nuevoPerfil) => {
